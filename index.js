6e4bfa83480e37405e9113f68463f57052dc569d
Oliver <1155175373@link.cuhk.edu.hk>
2022-07-11 19:28:17 +0800

    ready to push to github

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios").default;
+const fs = require("fs-extra");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/:name", (req, res) => {
+  const keys = fs.readJSONSync("./DO_NOT_PUSH_TO_GITHUB.json");
+  const api_key = keys[req.params.name];
+  const URL = req.body.URL.replace("api_key", api_key);
+
+  axios.get(URL).then((axiosRes) => res.send(axiosRes.data));
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
